a85955490aafb8ff9099a0aa107e8de65c04fdca
luanalbert <contatoluanalbert@gmail.com>
2020-07-07 20:05:30 -0300

    correções feitas

--- js/script.js
+++ js/script.js
@@ -3,7 +3,9 @@ function calcular(tipo, valor) {
         if(valor === 'c'){
            // limpar o visor
            document.getElementById('resultado').value = '' 
-           document.getElementById('resultado').style.background = ('') 
+           document.getElementById('resultado').style.background = ('')
+           document.getElementById('resultado').style.color = ('#000000 ') 
+           
         }
         if(valor === '+' || valor === '-' || valor === '*' || valor === '/' ||valor === '.') {
             document.getElementById('resultado').value += valor
@@ -12,6 +14,13 @@ function calcular(tipo, valor) {
            var valorCampo = eval(document.getElementById('resultado').value)
             document.getElementById('resultado').value = valorCampo
             document.getElementById('resultado').style.background = ('#7FFFD4')
+            
+            if (valorCampo == undefined) {//alertas caso os valores sejam indefinidos
+                document.getElementById('resultado').value = "Operação não suportada"
+                document.getElementById('resultado').style.background = ('#FF0000')
+                document.getElementById('resultado').style.color = ('#FFFFFF')
+                
+            }
         }
     }else if (tipo === 'valor') {
     
